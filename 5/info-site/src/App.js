4d4ece62731fb361ca83cd82fd20483186db9ba6
shubh.johri <shubh.johri@torinit.ca>
2022-04-13 21:41:55 +0530

    react basic structure

---
+++ 5/info-site/src/App.js
@@ -0,0 +1,6 @@
+import NavBar from './components/NavBar/NavBar'
+import Main from './components/Main/Main'
+
+export default function App () {
+    return <div> <NavBar /> <Main /> </div>
+}
---
+++ 5/info-site/src/components/Main/Main.js
@@ -0,0 +1,3 @@
+export default function Main() {
+  return (<div><h1>Main</h1></div>)
+}
